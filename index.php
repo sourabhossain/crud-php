4494d9e90bb215bcb011c1391e8d04d90c1adc5e
Sourab Hossain <sourabhossain.cse@gmail.com>
2021-04-29 09:14:20 +0600

    read data

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ if ( 'seed' == $task ) {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Form Example</title>
+    <title>CRUD FOR PHP</title>
     <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
     <link rel="stylesheet" href="//cdn.rawgit.com/necolas/normalize.css/master/normalize.css">
     <link rel="stylesheet" href="//cdn.rawgit.com/milligram/milligram/master/dist/milligram.min.css">
